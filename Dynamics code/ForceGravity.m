e26cf0953fd03a048af4f3c9b141754302cf8488
LeafarCoder <jose.rafael1997@hotmail.com>
2019-12-23 16:11:37 +0000

    ForceGravity name correction

---
+++ Dynamics code/ForceGravity.m
@@ -0,0 +1,14 @@
+function [g] = ForceGravity()
+
+global Body NBodies grav NCoordinates
+
+% initialize the force vector
+g = zeros(NCoordinates,1);
+
+% include the gravity force for every body
+for i=1:NBodies
+    i1 = 3*i - 2;
+    i2 = i1 + 1;
+    g(i1:i2,1) = Body(i).mass*grav;
+end
+end
