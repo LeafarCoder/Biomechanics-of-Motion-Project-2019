1be0eb94e51ae783484b8e1095cf79b5a5719954
Capsized <59203090+Capsized@users.noreply.github.com>
2019-12-27 00:59:36 +0000

    Working on ForceContact
    Working on ForceContact - to be finished

--- Dynamics code/ForceContact.m
+++ Dynamics code/ForceContact.m
@@ -1,6 +1,8 @@
 function [g] = ForceContact(g,t)
 
-global NFPlate FPlate NBody Body
+global NFPlate FPlate Body
+
+%%%%%%TO BE FINISHED - PLEASE DO NOT CHANGE%%%%%%%%
 
 %for each ground force
 for k=1:NFPlate
@@ -10,7 +12,28 @@ for k=1:NFPlate
     Fz =    ppval(FPlate(k).Fz_spline, t);
     CoPxy = ppval(FPlate(k).COPxy_spline, t);
     
+    %Simplifying variable use
+    i=FPlate(k).i;
+    j=FPlate(k).j;
+    spi=FPlate(k).spi;
+    spj=FPlate(k).spj;
+    
+    %Deciding in which body to apply the force
     force=[Fxy; Fz];
+    Boundary_Point = Body(i).r + Body(i).A * spi;
+    
+    if CoPxy < Boundary_Point
+        %apply force to body i
+        [g] = ApplyForce(i,g,force,spi)
+    else
+        %apply force to body j
+        [g] = ApplyForce(j,g,force,spj)
+        
+    end
+    
+        
+    
+    
     
     [force,sPpi] = groundreaction(k,t);
     spi = Body(i).A*sPpi;
