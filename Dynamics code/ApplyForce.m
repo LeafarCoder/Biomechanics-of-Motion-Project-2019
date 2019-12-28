a87c4642cbe5bbc5aea09cb5f88ecc994501c8d6
Capsized <59203090+Capsized@users.noreply.github.com>
2019-12-28 13:08:32 +0000

    Force Contact finished
    Force Contact finished

--- Dynamics code/ApplyForce.m
+++ Dynamics code/ApplyForce.m
@@ -1,6 +1,5 @@
 function [g] = ApplyForce(i,g,force,sp)
 
-
 i1 = 3*i-2;
 i2 = i1 + 1;
 i3 = i2 + 1;
--- Dynamics code/ForceContact.m
+++ Dynamics code/ForceContact.m
@@ -2,44 +2,36 @@ function [g] = ForceContact(g,t)
 
 global NFPlate FPlate Body
 
-%%%%%%TO BE FINISHED - PLEASE DO NOT CHANGE%%%%%%%%
-
 %for each ground force
 for k=1:NFPlate
 
-    %Evaluate the splines of Forces and COP at time t
+    %Evaluate the splines of Forces and COP at time t 
     Fxy =   ppval(FPlate(k).Fxy_spline, t);
     Fz =    ppval(FPlate(k).Fz_spline, t);
     CoPxy = ppval(FPlate(k).COPxy_spline, t);
-    
+    CoPz = ppval(FPlate(k).COPz_spline, t);
     %Simplifying variable use
     i=FPlate(k).i;
     j=FPlate(k).j;
-    spi=FPlate(k).spi;
-    spj=FPlate(k).spj;
+    spiB=FPlate(k).spi;
     
+    %Applys to the right body the force and corresponding Moment
+    
+    Force=[Fxy; Fz];
     %Deciding in which body to apply the force
-    force=[Fxy; Fz];
-    Boundary_Point = Body(i).r + Body(i).A * spi;
+    Boundary_Point = Body(i).r + Body(i).A * spiB;
     
     if CoPxy < Boundary_Point
         %apply force to body i
-        [g] = ApplyForce(i,g,force,spi)
+        Rcm=[CoPxy;CoPz]-Body(i).r;
+        [g] = ApplyForce(i,g,Force,Rcm);
     else
         %apply force to body j
-        [g] = ApplyForce(j,g,force,spj)
+        Rcm=[CoPxy;CoPz]-Body(j).r;
+        [g] = ApplyForce(j,g,Force,Rcm);
         
     end
-    
-        
-    
-    
-    
-    [force,sPpi] = groundreaction(k,t);
-    spi = Body(i).A*sPpi;
-    
-    %Applys to the right body the force and corresponding Moment
-    [g] = ApplyForce(i,g,force,n,sp);
+
 end
 
 end
